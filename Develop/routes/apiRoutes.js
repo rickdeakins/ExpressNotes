37af255fa5284d9970f17ab8d14134fa65d488ab
rickdeakins <rickdeakinsjr@gmail.com>
2023-11-15 22:08:43 -0500

    debugging

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path');
+
 const router = express.Router();
+const  { readFromFile, writeToFile, readAndAppend } = require('../utils/helper');
 
 //GET route for /api/notes to read the `db.json` file and return all saved notes as JSON.
-router.get('/api/notes', async (req, res) => {
-    try {const data = await readFromFile('./db/db.json', 'utf8'); const parsedNotes = JSON.parse(data);res.json(parsedNotes);
+router.get('/notes', async (req, res) => {
+    try {
+      console.log('pwd = ', __dirname);
+      const data = await readFromFile(path.join(__dirname, '../db/db.json'), 'utf8'); 
+      console.log('data = ', data);
+    const parsedNotes = JSON.parse(data);res.json(parsedNotes);
      } catch (err) {console.error(err);res.status(500).json({ error: 'Internal Server Error' });}});
    
 //POST route for /api/notes to receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the user.
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
 console.info(`${req.method} request received to add a note.`); })// Log that a POST request was received
    
 module.exports = router;
