fd19863290d53f9ab38504fba363a75e4269439e
rickdeakins <rickdeakinsjr@gmail.com>
2023-11-15 21:49:25 -0500

    updated route directory

--- Develop/routes/htmlroutes.js
+++ Develop/routes/htmlroutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 //GET route for notes.html page
-router.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+router.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
 // GET route for index.html page
-router.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+router.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 module.exports = router;
