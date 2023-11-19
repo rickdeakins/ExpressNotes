0ba41710c2766ef84106fc4635d6effc35d476b9
rickdeakins <rickdeakinsjr@gmail.com>
2023-11-19 16:07:27 -0500

    revised order of dependencies

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 //Required dependencies
 const express = require("express");
 
-// Initialize Express instance
-const app = express();
-
-// Specify the port the Express server will run
-const PORT = process.env.PORT || 3001;
-
 //require files
 const apiRoutes = require("./Develop/routes/apiRoutes");
 const htmlRoutes = require("./Develop/routes/htmlRoutes");
 
+// Initialize Express instance
+const app = express();
+
+// Specify the port the Express     server will run
+const PORT = process.env.PORT || 3001;
+
 //Middleware - how Express handles data parsing
-app.use(express.json()); //Parsing JSON data
+app.use(ex  press.json()); //Parsing JSON data
 app.use(express.urlencoded({ extended: true })); //Handle form submissions and and access form data in Express routes
 
 app.use("/api", apiRoutes);
