81e72172576bdbd573442eff8bb90baf66b98689
rickdeakins <rickdeakinsjr@gmail.com>
2023-11-19 14:23:22 -0500

    revised port

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 //Required dependencies
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const { v4: uuidv4 } = require('uuid');
-
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const { v4: uuidv4 } = require("uuid");
 
 // Initialize Express instance
 const app = express();
@@ -13,21 +12,21 @@ const app = express();
 const readFromFile = util.promisify(fs.readFile);
 
 // Specify the port the Express server will run
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
-//require files 
-const apiRoutes = require('./Develop/routes/apiRoutes');
-const htmlRoutes = require('./Develop/routes/htmlRoutes');
+//require files
+const apiRoutes = require("./Develop/routes/apiRoutes");
+const htmlRoutes = require("./Develop/routes/htmlRoutes");
 
-
-
-//Middleware - how Express handles data parsing 
-app.use(express.static('public')); //Serving static files from public directory
+//Middleware - how Express handles data parsing
+app.use(express.static("public")); //Serving static files from public directory
 app.use(express.json()); //Parsing JSON data
 app.use(express.urlencoded({ extended: true })); //Handle form submissions and and access form data in Express routes
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 //Server listener
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
